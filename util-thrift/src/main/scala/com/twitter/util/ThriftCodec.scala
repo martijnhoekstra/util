ecaf12d0423681ae75c25f9ffd759deef23b02f2
Martijn Hoekstra <martijnhoekstra@gmail.com>
2020-05-11 11:25:07 +0200

    Manifest -> ClassTag

--- util-thrift/src/main/scala/com/twitter/util/ThriftCodec.scala
+++ util-thrift/src/main/scala/com/twitter/util/ThriftCodec.scala
@@ -1,22 +1,21 @@
 package com.twitter.util
 
+import scala.reflect.ClassTag
 import org.apache.thrift.TBase
 import org.apache.thrift.protocol.{TBinaryProtocol, TCompactProtocol, TProtocolFactory}
 
 object ThriftCodec {
-  def apply[T <: TBase[_, _]: Manifest, P <: TProtocolFactory: Manifest]: ThriftCodec[T, P] =
+  def apply[T <: TBase[_, _], P <: TProtocolFactory](implicit ctt: ClassTag[T], ctp: ClassTag[P]): ThriftCodec[T, P] =
     new ThriftCodec[T, P]
 }
 
-class ThriftCodec[T <: TBase[_, _]: Manifest, P <: TProtocolFactory: Manifest]
+class ThriftCodec[T <: TBase[_, _], P <: TProtocolFactory](implicit ctt: ClassTag[T], ctp: ClassTag[P])
     extends Codec[T, Array[Byte]]
     with ThriftSerializer {
 
-  protected lazy val prototype: T =
-    manifest[T].runtimeClass.asInstanceOf[Class[T]].newInstance
+  protected lazy val prototype: T = ctt.runtimeClass.asInstanceOf[Class[T]].newInstance
 
-  lazy val protocolFactory: TProtocolFactory =
-    manifest[P].runtimeClass.asInstanceOf[Class[P]].newInstance
+  lazy val protocolFactory: TProtocolFactory = ctp.runtimeClass.asInstanceOf[Class[P]].newInstance
 
   def encode(item: T): Array[Byte] = toBytes(item)
 
@@ -27,7 +26,7 @@ class ThriftCodec[T <: TBase[_, _]: Manifest, P <: TProtocolFactory: Manifest]
   }
 }
 
-class BinaryThriftCodec[T <: TBase[_, _]: Manifest] extends ThriftCodec[T, TBinaryProtocol.Factory]
+class BinaryThriftCodec[T <: TBase[_, _]: ClassTag] extends ThriftCodec[T, TBinaryProtocol.Factory]
 
-class CompactThriftCodec[T <: TBase[_, _]: Manifest]
+class CompactThriftCodec[T <: TBase[_, _] : ClassTag]
     extends ThriftCodec[T, TCompactProtocol.Factory]
